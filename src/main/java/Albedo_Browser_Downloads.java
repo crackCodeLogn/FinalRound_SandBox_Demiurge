b82d471db576d33ab4b6d8a91b04807ddb6be068
crackCodeLogn <vivektheintel@gmail.com>
2018-04-01 23:45:35 +0530

    commit -- final download sandboxed part in place

--- src/main/java/Albedo_Browser_Downloads.java
+++ src/main/java/Albedo_Browser_Downloads.java
@@ -16,7 +16,17 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import javafx.util.Callback;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,16 +42,17 @@ public class Albedo_Browser_Downloads extends Application {
         int number;
         String title;
         String downloadTimeStamp;
+        long downloadTimeStampLong;
         boolean commitStatus;
 
         public DownloadRecord() {
-
         }
 
-        public DownloadRecord(int number, String title, String downloadTimeStamp) {
+        public DownloadRecord(int number, String title, String downloadTimeStamp, long downloadTimeStampLong) {
             this.number = number;
             this.title = title;
             this.downloadTimeStamp = downloadTimeStamp;
+            this.downloadTimeStampLong = downloadTimeStampLong;
         }
 
         public int getNumber() {
@@ -75,6 +86,14 @@ public class Albedo_Browser_Downloads extends Application {
         public void setCommitStatus(boolean commitStatus) {
             this.commitStatus = commitStatus;
         }
+
+        public long getDownloadTimeStampLong() {
+            return downloadTimeStampLong;
+        }
+
+        public void setDownloadTimeStampLong(long downloadTimeStampLong) {
+            this.downloadTimeStampLong = downloadTimeStampLong;
+        }
     }
 
     private class ButtonCell extends TableCell<DownloadRecord, Boolean> {
@@ -90,16 +109,28 @@ public class Albedo_Browser_Downloads extends Application {
                 @Override
                 public void handle(ActionEvent t) {
                     // do something when button clicked
-                    //...
-                    System.out.println("fileLocationMapper : "+ fileLocationMapper);
+                    System.out.println("fileLocationMapper : " + fileLocationMapper);
                     System.out.println("Firing up from the button click of commit, src : " + t.getSource());
                     //System.out.println("\tlocalcounter : "+localCounter+", fileLocationMapper val : "+fileLocationMapper.get(localCounter));
                     //the above approach failed...so trying a new format of fileLocationMapper and button
                     //the localCounter is apt only for the time of init the buttons, and not for accessing them afterwards
 
                     Button caller = (Button) t.getSource();
-                    System.out.println("Button mapping : "+(buttonMapper.get(caller)));
-                    System.out.println("\tFile loc mapping : "+(fileLocationMapper.get(buttonMapper.get(caller))));
+                    System.out.println("Button mapping : " + (buttonMapper.get(caller)));
+                    File srcFile = fileLocationMapper.get(buttonMapper.get(caller));
+                    System.out.println("\tFile loc mapping : " + srcFile.getName() + " --> " + srcFile.toPath());
+
+                    System.out.println("Firing the file commiting ~ transferring");
+                    cellButton.setDisable(true);
+
+                    try {
+                        Files.copy(srcFile.toPath(), Paths.get(downloadFileDirectory_OutBoxPath + srcFile.getName()), StandardCopyOption.REPLACE_EXISTING);
+                        srcFile.delete();
+                        System.out.println("Completed file transfer and deleted the source file!");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.out.println("FAILURE IN COPYING / DELETING THE FILE WHICH REQUESTED COMMIT");
+                    }
                 }
             });
         }
@@ -117,9 +148,12 @@ public class Albedo_Browser_Downloads extends Application {
     private static ObservableList<DownloadRecord> data = null;
     //FXCollections.observableArrayList(new DownloadRecord(1, "Titleeeeeeeeeeeeeeeeeeeeeeeeeee", "Visited Time"));
 
-    static int localCounter=0;
-    static Map<Integer, String> fileLocationMapper = new HashMap<>();
+    static int localCounter = 0;
+    static Map<Integer, File> fileLocationMapper = new HashMap<>();
     static Map<Button, Integer> buttonMapper = new HashMap<>();
+
+    private static String downloadFileDirectoryPath = "e:\\sandbox_target\\tmp\\";
+    private static String downloadFileDirectory_OutBoxPath = "e:\\sandbox_outbox\\";
 
     public static void fillData() {
 
@@ -128,15 +162,49 @@ public class Albedo_Browser_Downloads extends Application {
 
         localCounter = -2;
 
+        File file = new File(downloadFileDirectoryPath);
+        File filesList[] = file.listFiles();
+
+        for (File f1 : filesList) {
+            System.out.println("File name --> " + f1);
+
+            DownloadRecord node = new DownloadRecord();
+            node.setNumber(tr);
+            node.setTitle(f1.getName());
+
+            try {
+                //Path localFile = Paths.get("resources/RESIZED_ic_refresh_black_48dp_2x.png");
+                Path localFile = Paths.get(f1.getPath());
+                BasicFileAttributes attributes = Files.readAttributes(localFile, BasicFileAttributes.class);
+                System.out.println("File creation time : " + new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format((attributes.lastModifiedTime().toMillis())));
+                System.out.println("file ccr : " + attributes.lastModifiedTime().toMillis());
+
+                node.setDownloadTimeStamp(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format((attributes.lastModifiedTime().toMillis())));
+                node.setDownloadTimeStampLong(attributes.lastModifiedTime().toMillis());
+            } catch (Exception e) {
+                System.out.println("Error encountered while reading file attribute...\n\tException" + e);
+                node.setDownloadTimeStamp("N.A.");
+                node.setDownloadTimeStampLong(0);
+            }
+
+            node.setCommitStatus(false);
+
+            if (data == null) data = FXCollections.observableArrayList(node);
+            else data.add(node);
+
+            //mapping the number to the file downloaded
+            fileLocationMapper.put(tr, f1);
+
+            tr++;
+        }
+
+        /*
         DownloadRecord node = new DownloadRecord();
         node.setNumber(1);
         node.setTitle("first.pdf");
         node.setDownloadTimeStamp(String.valueOf(new Timestamp(System.currentTimeMillis())));
         node.setCommitStatus(false);
         //localCounter++;
-
-        if (data == null) data = FXCollections.observableArrayList(node);
-        else data.add(node);
 
         node = new DownloadRecord();
         node.setNumber(2);
@@ -146,15 +214,33 @@ public class Albedo_Browser_Downloads extends Application {
         //localCounter++;
 
         data.add(node);
+        */
+    }
 
-        fileLocationMapper.put(1, "firstLink");
-        fileLocationMapper.put(2, "secondLink");
+    public static void clearDownloads() {
+        //wiping out the downloads folder
+        File file = new File(downloadFileDirectoryPath);
+        File filesList[] = file.listFiles();
+
+        int i = 0;
+        //File filesListArray[] = new File[filesList.length];
+        for (File f1 : filesList) {
+            System.out.println("\t\tFile " + (++i) + " to be deleted : " + f1);
+            f1.delete();
+            //filesListArray[i++] = f1;
+        }
+        /*
+        for(i=0;i<filesListArray.length;i++){
+            filesListArray[i].delete();
+        }
+        */
+        System.out.println("Exiting the clear Downloads function");
     }
 
     @Override
     public void start(Stage primaryStage) {
 
-        primaryStage.setTitle("DONWLOADS");
+        primaryStage.setTitle("DOWNLOADS");
         primaryStage.setWidth(800);
         VBox vBox = new VBox();
 
@@ -188,6 +274,19 @@ public class Albedo_Browser_Downloads extends Application {
                 });
 
         table.getColumns().addAll(recordNumberCol, siteNameCol, lastVisitedTimeCol, commitButtonCol);
+
+        Collections.sort(data, new Comparator<DownloadRecord>() {
+            @Override
+            public int compare(DownloadRecord o1, DownloadRecord o2) {
+                return (int) (o2.getDownloadTimeStampLong() - o1.getDownloadTimeStampLong());
+            }
+        });
+
+        int tr = data.size();
+        for (DownloadRecord record : data) {
+            record.setNumber(tr--);
+        }
+
         table.setItems(data);
 
         vBox.getChildren().addAll(table);
