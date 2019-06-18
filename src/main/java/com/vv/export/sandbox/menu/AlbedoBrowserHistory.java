ba6c2e4a461807f16af7bc1768b9096463c6cb95
crackCodeLogN <taekwondo@Dan1>
2019-06-19 00:50:21 +0530

    Refactoring the History logic

--- src/main/java/com/vv/export/sandbox/menu/AlbedoBrowserHistory.java
+++ src/main/java/com/vv/export/sandbox/menu/AlbedoBrowserHistory.java
@@ -4,7 +4,6 @@ import com.vv.export.sandbox.AlbedoBrowser;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -13,7 +12,9 @@ import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebHistory;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 /**
@@ -23,95 +24,53 @@ import javafx.stage.WindowEvent;
  */
 public class AlbedoBrowserHistory extends Application {
 
-    public static class HistoryRecord {
-        int number;
-        String title;
-        String visitTimeStamp;
-
-        public HistoryRecord() {
-        }
-
-        public HistoryRecord(int number, String title, String visitTimeStamp) {
-            this.number = number;
-            this.title = title;
-            this.visitTimeStamp = visitTimeStamp;
-        }
-
-        public int getNumber() {
-            return number;
-        }
-
-        public void setNumber(int number) {
-            this.number = number;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        public String getVisitTimeStamp() {
-            return visitTimeStamp;
-        }
-
-        public void setVisitTimeStamp(String visitTimeStamp) {
-            this.visitTimeStamp = visitTimeStamp;
-        }
-    }
-
-    private static ObservableList<HistoryRecord> data = null;
-    //FXCollections.observableArrayList(new DownloadRecord(1, "Titleeeeeeeeeeeeeeeeeeeeeeeeeee", "Visited Time"));
-
     public static void fillData(WebEngine webEngine) {
-        WebHistory history = webEngine.getHistory();
-        ObservableList<WebHistory.Entry> list = history.getEntries();
-        int tr = 1;
-        if(data!=null) data.clear();
+        final ObservableList<WebHistory.Entry> list = webEngine.getHistory().getEntries();
+        final AtomicInteger recordNumber = new AtomicInteger(1);
+        if (Objects.nonNull(data)) data.clear();
 
-        for (WebHistory.Entry entry : list) {
-            System.out.println("Entry " + tr + " --> " + entry.getTitle() + " :: " + entry.getUrl() + " :: " + entry.getLastVisitedDate());
+        list.forEach(entry -> {
+            System.out.println("Entry " + recordNumber.get() + " --> " + entry.getTitle() + " :: " + entry.getUrl() + " :: " + entry.getLastVisitedDate());
 
-            HistoryRecord node = new HistoryRecord();
-            node.setNumber(tr);
+            final HistoryRecord node = new HistoryRecord();
+            node.setNumber(recordNumber.getAndIncrement());
             try {
                 node.setTitle(entry.getTitle());
-            } catch (NullPointerException e1){
+            } catch (NullPointerException e1) {
                 node.setTitle("null");
             }
             try {
                 node.setVisitTimeStamp(entry.getLastVisitedDate().toString());
-            } catch (NullPointerException e1){
+            } catch (NullPointerException e1) {
                 node.setVisitTimeStamp("null");
             }
 
-            if (data == null) {
+            if (Objects.isNull(data)) {
                 data = FXCollections.observableArrayList(node);
             } else {
                 data.add(node);
             }
-
-            tr++;
-        }
+        });
     }
+
+    private static ObservableList<HistoryRecord> data = null;
+    //FXCollections.observableArrayList(new DownloadRecord(1, "Titleeeeeeeeeeeeeeeeeeeeeeeeeee", "Visited Time"));
 
     @Override
     public void start(Stage primaryStage) {
 
         primaryStage.setTitle("HISTORY");
         primaryStage.setWidth(800);
-        VBox vBox = new VBox();
+        final VBox vBox = new VBox();
 
-        TableView table = new TableView();
+        final TableView table = new TableView();
         table.setEditable(false);
 
-        TableColumn recordNumberCol = new TableColumn("Record number");
+        final TableColumn recordNumberCol = new TableColumn("Record number");
         recordNumberCol.setCellValueFactory(new PropertyValueFactory<>("number"));
-        TableColumn siteNameCol = new TableColumn("Site name");
+        final TableColumn siteNameCol = new TableColumn("Site name");
         siteNameCol.setCellValueFactory(new PropertyValueFactory<>("title"));
-        TableColumn lastVisitedTimeCol = new TableColumn("Time of visit");
+        final TableColumn lastVisitedTimeCol = new TableColumn("Time of visit");
         lastVisitedTimeCol.setCellValueFactory(new PropertyValueFactory<>("visitTimeStamp"));
 
         table.getColumns().addAll(recordNumberCol, siteNameCol, lastVisitedTimeCol);
@@ -121,12 +80,31 @@ public class AlbedoBrowserHistory extends Application {
         primaryStage.setScene(new Scene(vBox));
         primaryStage.show();
 
-        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            public void handle(WindowEvent we) {
-                System.out.println("Stage is closing");
-                AlbedoBrowser.historyFrameOpen = false;
-            }
+        primaryStage.setOnCloseRequest(windowEvent -> {
+            System.out.println("Stage is closing");
+            AlbedoBrowser.historyFrameOpen = false;
         });
+    }
+
+    public static class HistoryRecord {
+        int number;
+        String title;
+        String visitTimeStamp;
+
+        HistoryRecord() {
+        }
+
+        void setNumber(int number) {
+            this.number = number;
+        }
+
+        void setTitle(String title) {
+            this.title = title;
+        }
+
+        void setVisitTimeStamp(String visitTimeStamp) {
+            this.visitTimeStamp = visitTimeStamp;
+        }
     }
 
     public static void main(String[] args) {
